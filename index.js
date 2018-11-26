8b0b4e15ff0ee754ef24d456e308dbc3c99611df
Chen Wen <joc98@163.com>
2018-11-26 15:11:41 +0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,82 @@
+const {Client, config: ggConfig} = require('gnat-grpc');
+const express = require('express');
+const URL = require('url');
+const bodyParser = require('body-parser');
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const camelCase = require('lodash.camelcase');
+
+const doResponse = (res, statusCode, json) => {
+  res.writeHead(statusCode, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify(json));
+};
+
+const getRouteHandler = grpcClient => {
+  const fn = async (req, res) => {
+    let {serviceName, methodName} = req.params;
+    const {args, metadata, callOpts} = req.body;
+    const service = grpcClient.getService(serviceName);
+    const unimplemented = () =>
+      doResponse(res, 500, {error: {code: grpc.status.UNIMPLEMENTED, details: 'Unimplemented'}});
+    if (!service) {
+      return unimplemented();
+    }
+    if (!service[methodName]) {
+      methodName = camelCase(methodName);
+    }
+    if (!service[methodName]) {
+      return unimplemented();
+    }
+
+    const argArr = callOpts ? [args, metadata || {}, callOpts] : [args, metadata, callOpts].filter(arg => arg);
+
+    try {
+      const result = await service[methodName](...argArr);
+      console.log(result);
+      doResponse(res, 200, {result});
+    } catch (error) {
+      doResponse(res, 500, {error});
+      console.error(`${serviceName}/${methodName}`, error.stack);
+    }
+  };
+  return async (req, res) => {
+    try {
+      await fn(req, res);
+    } catch (e) {
+      console.error(e.stack);
+      doResponse(req, 500, {error: e.toObject()});
+    }
+  };
+};
+
+const useRoute = (httpProxy, grpcClient) => httpProxy.use('/:serviceName/:methodName', getRouteHandler(grpcClient));
+
+const initHttpProxy = ({httpProxy}) => {
+  if (!httpProxy) {
+    httpProxy = express.Router();
+
+    httpProxy.use(bodyParser.json());
+    httpProxy.use(bodyParser.urlencoded({extended: false}));
+  }
+  return httpProxy;
+};
+
+const initGrpcClient = ({grpcClient = {}}) => {
+  if (grpcClient instanceof Client) {
+    return grpcClient;
+  }
+
+  const conf = grpcClient;
+  ggConfig(Object.assign({grpc, protoLoader}, conf.ggConf));
+
+  const {clientConf} = conf;
+  grpcClient = Client.checkoutServicesSync(clientConf);
+  return grpcClient;
+};
+
+module.exports = function (conf = {}) {
+  const httpProxy = initHttpProxy(conf);
+  const grpcClient = initGrpcClient(conf);
+  useRoute(httpProxy, grpcClient);
+  return httpProxy;
+};
---
+++ test/proxy-test.js
@@ -0,0 +1,111 @@
+const GG = require('gnat-grpc');
+const PATH = require('path');
+const grpc = require('@grpc/grpc-js');
+const {expect} = require('chai');
+const watcher = require('../');
+const request = require('supertest');
+const {random, lorem} = require('faker');
+const express = require('express');
+
+GG.config({
+  grpc: require('grpc'),
+  protoLoader: require('@grpc/proto-loader'),
+  root: PATH.join(__dirname, '.proto'),
+});
+const {Client, Server} = GG;
+const app = express();
+
+const postRequest = () => {
+  const agent = request.agent(app);
+  return ({url, body, expectation}) =>
+    agent.post(url)
+      .send(body)
+      .expect('Content-Type', /json/)
+      .expect(expectation.statusCode);
+};
+
+let PORT = 50057;
+const throwAnErr = ({name}) => {
+  const err = new Error(`使用了错误的名字 "${name}"，写错了写错了写错了写错了写错了写错了写错了写错了`);
+  err.code = 20000;
+  throw err;
+};
+
+let asserts = [];
+const sayHello = function ({name}) {
+  asserts.forEach(cb => cb.call(this, ...Array.prototype.slice.call(arguments, 0)));
+  return {message: `Hello ${name}`, testExField: '1111'};
+};
+
+describe('watcher', () => {
+  let server;
+  let client;
+  let proxy;
+  let req;
+  let baseUrl;
+
+  beforeEach(async () => {
+    server = await Server.addServer({
+      bindPath: `0.0.0.0:${PORT}`,
+      services: [
+        {filename: 'helloworld.proto'},
+      ],
+      methods: {
+        'gnat.helloworld.Greeter': {sayHello, throwAnErr},
+      }
+    });
+
+    server.start();
+  });
+
+  beforeEach(async () => {
+    client = await Client.checkoutServices({
+      bindPath: `localhost:${PORT}`,
+      services: [
+        {filename: 'helloworld.proto'},
+      ]
+    });
+  });
+
+  beforeEach(() => {
+    baseUrl = `/${lorem.word()}`;
+    proxy = watcher({grpcClient: client});
+    app.use(baseUrl, proxy);
+    req = postRequest();
+  });
+
+  afterEach(() => server.tryShutdown());
+  afterEach(() => client.close());
+
+  it('proxy', async () => {
+    const name = random.word();
+    const ret = await req({
+      url: `${baseUrl}/gnat.helloworld.Greeter/SayHello`,
+      body: {args: {name}},
+      expectation: {statusCode: 200}
+    });
+    expect(ret).to.have.property('body').to.deep.equal({result: {message: `Hello ${name}`}});
+  });
+
+  it('when deadline exceeded', async () => {
+    const name = random.word();
+    const ret = await req({
+      url: `${baseUrl}/gnat.helloworld.Greeter/SayHello`,
+      body: {args: {name}, callOpts: {deadline: 0}},
+      expectation: {statusCode: 500}
+    });
+    expect(ret).to.have.deep.nested.property('body.error.code').to.equal(grpc.status.DEADLINE_EXCEEDED);
+    expect(ret).to.have.deep.nested.property('body.error.details').to.equal('Deadline Exceeded');
+  });
+
+  it('on respond custom error', async () => {
+    const name = random.word();
+    const ret = await req({
+      url: `${baseUrl}/gnat.helloworld.Greeter/ThrowAnErr`,
+      body: {args: {name}},
+      expectation: {statusCode: 500}
+    });
+    expect(ret).to.have.deep.nested.property('body.error.code').to.equal(20000);
+    expect(ret).to.have.deep.nested.property('body.error.details').to.equal(`使用了错误的名字 "${name}"，写错了写错了写错了写错了写错了写错了写错了写错了`);
+  });
+});
